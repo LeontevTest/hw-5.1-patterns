656df6d7861dd728cb7dc1567b17e0059566c452
Aleks test <7501130l@gmail.com>
2024-06-03 16:24:38 +0300

    Initial commit

--- src/test/java/test/DeliveryTest.java
+++ src/test/java/test/DeliveryTest.java
@@ -1,10 +1,10 @@
 package test;
 
 import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import data.DataGenerator;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
 
 
@@ -20,6 +20,15 @@ class DeliveryTest {
     @BeforeEach
     void setup() {
         open("http://localhost:9999");
+    }
+
+    @BeforeAll
+    static void setUpAll() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
     }
 
     @Test
